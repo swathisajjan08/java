40035d45f5606455cf2fb7fea7bcba15eba52421
swathi sajjan <swathi.novaglobal.in>
2025-06-02 15:49:16 +0530

    stages made changes

--- stages/stages.js
+++ stages/stages.js
@@ -12,118 +12,105 @@ const logPromise = (message) => {
   });
 };
 
-const step3Promise = (t1) => {
+const step3Promise = (delay) => {
   return new Promise((thirdResolve, reject) => {
-    const t1 = Date.now() % 2000;
     setTimeout(() => {
-      thirdResolve(`Waiting for t1 time ${t1}ms  `);
-    }, t1);
+      thirdResolve(`Waiting for time ${delay}ms`);
+    }, delay);
   });
 };
 
 const stage1Promise = () => {
   return new Promise((resolve, reject) => {
-    delayPromise(1200).then((message) => {
+    delayPromise(500).then((message) => {
       console.log(message);
       logPromise("STAGE1(2) - Going to fetch from dev db").then((message) => {
         console.log(message);
-        step3Promise()
-          .then((message) => {
-            console.log(message);
-          })
-          .then(() => {
-            resolve("stage 1 is resolved");
-          });
+        const t1 = Date.now() % 2000;
+        step3Promise(t1).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 1 is resolved", t1 });
+        });
       });
     });
   });
 };
 
-const stage2Promise = () => {
+const stage2Promise = (t1) => {
   return new Promise((resolve, reject) => {
-    step3Promise().then((message) => {
+    step3Promise(t1).then((message) => {
       console.log(message);
       logPromise("STAGE 2(2)- fetching the plant").then((message) => {
         console.log(message);
-        step3Promise()
-          .then((message) => {
-            console.log(message);
-          })
-          .then(() => {
-            resolve("stage 2 is resolved");
-          });
+        const t2 = Date.now() % 2000;
+        step3Promise(t2).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 2 is resolved", t2 });
+        });
       });
     });
   });
 };
 
-const stage3Promise = () => {
+const stage3Promise = (t2) => {
   return new Promise((resolve, reject) => {
-    step3Promise().then((message) => {
+    step3Promise(t2).then((message) => {
       console.log(message);
       logPromise("STAGE 3(2)- finished fetching the plant").then((message) => {
         console.log(message);
-        step3Promise()
-          .then((message) => {
-            console.log(message);
-          })
-          .then(() => {
-            resolve("stage 3 is resolved");
-          });
+        const t3 = Date.now() % 2000;
+        step3Promise(t3).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 3 is resolved", t3 });
+        });
       });
     });
   });
 };
 
-const stage4Promise = () => {
+const stage4Promise = (t3) => {
   return new Promise((resolve, reject) => {
-    step3Promise().then((message) => {
+    step3Promise(t3).then((message) => {
       console.log(message);
       logPromise("STAGE 4(2)- Going to insert the plant").then((message) => {
         console.log(message);
-        step3Promise()
-          .then((message) => {
-            console.log(message);
-          })
-          .then(() => {
-            resolve("stage 4 is resolved");
-          });
+        const t4 = Date.now() % 2000;
+        step3Promise(t4).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 4 is resolved", t4 });
+        });
       });
     });
   });
 };
 
-const stage5Promise = () => {
+const stage5Promise = (t4) => {
   return new Promise((resolve, reject) => {
-    step3Promise().then((message) => {
+    step3Promise(t4).then((message) => {
       console.log(message);
       logPromise("STAGE 5(2)- Inserting the plant").then((message) => {
         console.log(message);
-        step3Promise()
-          .then((message) => {
-            console.log(message);
-          })
-          .then(() => {
-            resolve("stage 5 is resolved");
-          });
+        const t5 = Date.now() % 2000;
+        step3Promise(t5).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 5 is resolved", t5 });
+        });
       });
     });
   });
 };
 
-const stage6Promise = () => {
+const stage6Promise = (t5) => {
   return new Promise((resolve, reject) => {
-    step3Promise().then((message) => {
+    step3Promise(t5).then((message) => {
       console.log(message);
       logPromise("STAGE 6(2)- Finished inserting into mydb").then((message) => {
         console.log(message);
-        step3Promise()
-          .then((message) => {
-            console.log(message);
-          })
-          .then(() => {
-            resolve("stage 6 is resolved");
-          });
+        const t6 = Date.now() % 2000;
+        step3Promise(t6).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 6 is resolved" });
+        });
       });
     });
   });
@@ -131,21 +118,19 @@ const stage6Promise = () => {
 
 const stages = () => {
   return new Promise((resolve, reject) => {
-    stage1Promise().then((message) => {
+    stage1Promise().then(({ message: message, t1 }) => {
       console.log(message);
-      stage2Promise().then((message) => {
+      stage2Promise(t1).then(({ message: message, t2 }) => {
         console.log(message);
-        stage3Promise().then((message) => {
+        stage3Promise(t2).then(({ message: message, t3 }) => {
           console.log(message);
-          stage4Promise().then((message) => {
+          stage4Promise(t3).then(({ message: message, t4 }) => {
             console.log(message);
-            stage5Promise().then((message) => {
+            stage5Promise(t4).then(({ message: message, t5 }) => {
               console.log(message);
-              stage6Promise()
-                .then((message) => {
+              stage6Promise(t5)
+                .then(({ message: message }) => {
                   console.log(message);
-                })
-                .then(() => {
                   resolve("All stages are resolved");
                 })
                 .catch((error) => {
