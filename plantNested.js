8a63cbafdda83c96956b81b857322e3c10f92437
swathi sajjan <swathi.novaglobal.in>
2025-06-02 18:39:51 +0530

    plantNested made changes

--- plantNested.js
+++ plantNested.js
@@ -1,177 +1,244 @@
 import { createConnection } from "mysql2/promise";
 import fs from "fs";
-//Read DB config from JSON
+
 const config = fs.readFileSync("config_dbs.json", "utf-8");
 const configj = JSON.parse(config);
 
-const fetchdb = () =>{
-    return new Promise((resolve,reject)=>{
-        return createConnection(configj.dev_db)
-    })
-}
+const fetchFromDev = (id) => {
+  return new Promise((resolve, reject) => {
+    let conn;
+    createConnection(configj.dev_db)
+      .then((connection) => {
+        conn = connection;
+        return connection.execute(`SELECT * FROM mas_sites WHERE id = ?`, [id]);
+      })
+      .then(([rows]) => {
+        conn.close();
+        resolve(rows);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
 
+const insertIntoMydb = (c) => {
+  return new Promise((resolve, reject) => {
+    let conn;
+    createConnection(configj.my_db)
+      .then((connection) => {
+        conn = connection;
+        const sql = `INSERT INTO mas_sites (id,name,region_id_fk, asset_type_id_fk, created_on,created_by_fk,comm_date,lati,longi,unit_price,site_capacity,power_metric_fk,
+        address,spv_id_fk,utr_num,store_address,contact_num,fax_num,num_of_turbines,price_unit,feeder_id_fk,isDataCollectionEnabled,alias_name,timezone_id_fk,effeciency,
+        def_air_density,hcode,isMetMastAvailable) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE site_capacity = VALUES(site_capacity)`;
+        const values = [
+          c.id,
+          c.name,
+          c.region_id_fk,
+          c.asset_type_id_fk,
+          c.created_on,
+          c.created_by_fk,
+          c.comm_date,
+          c.lati,
+          c.longi,
+          c.unit_price,
+          c.site_capacity,
+          c.power_metric_fk,
+          c.address,
+          c.spv_id_fk,
+          c.utr_num,
+          c.store_address,
+          c.contact_num,
+          c.fax_num,
+          c.num_of_turbines,
+          c.price_unit,
+          c.feeder_id_fk,
+          c.isDataCollectionEnabled,
+          c.alias_name,
+          c.timezone_id_fk,
+          c.effeciency,
+          c.def_air_density,
+          c.hcode,
+          c.isMetMastAvailable,
+        ];
+        return connection.query(sql, values);
+      })
+      .then(() => {
+        conn.close();
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
 const delayPromise = (ms) => {
-    return new Promise((delayResolve, reject) => {
-      setTimeout(() => {
-        delayResolve(`Waiting for ${ms}ms before logging`);
-      }, ms);
-    });
-  };
-  
-  const logPromise = (message) => {
-    return new Promise((logResolve, reject) => {
-      logResolve(message);
-    });
-  };
-  
-  const step3Promise = (t1) => {
-    return new Promise((thirdResolve, reject) => {
-      const t1 = Date.now() % 2000;
-      setTimeout(() => {
-        thirdResolve(`Waiting for t1 time ${t1}ms  `);
-      }, t1);
-    });
-  };
-  
-  const stage1Promise = () => {
-    return new Promise((resolve, reject) => {
-      delayPromise(1200).then((message) => {
+  return new Promise((delayResolve, reject) => {
+    setTimeout(() => {
+      delayResolve(`Waiting for ${ms}ms before logging`);
+    }, ms);
+  });
+};
+
+const logPromise = (message) => {
+  return new Promise((logResolve, reject) => {
+    logResolve(message);
+  });
+};
+
+const step3Promise = (delay) => {
+  return new Promise((thirdResolve, reject) => {
+    setTimeout(() => {
+      thirdResolve(`Waiting for time ${delay}ms`);
+    }, delay);
+  });
+};
+
+const stage1Promise = () => {
+  return new Promise((resolve, reject) => {
+    delayPromise(500).then((message) => {
+      console.log(message);
+      logPromise("STAGE1(2) - Going to fetch from dev db").then((message) => {
         console.log(message);
-        logPromise("STAGE1(2) - Going to fetch from dev db").then((message) => {
+        const t1 = Date.now() % 2000;
+        step3Promise(t1).then((message) => {
           console.log(message);
-          step3Promise()
-            .then((message) => {
-              console.log(message);
-            })
-            .then(() => {
-              resolve("stage 1 is resolved");
-            });
+          resolve({ message: "stage 1 is resolved", t1 });
         });
       });
     });
-  };
-  
-  const stage2Promise = () => {
-    return new Promise((resolve, reject) => {
-      step3Promise().then((message) => {
-        console.log(message);
-        logPromise("STAGE 2(2)- fetching the plant").then((message) => {
-          console.log(message);
-          step3Promise()
-            .then((message) => {
-              console.log(message);
-            })
-            .then(() => {
-              resolve("stage 2 is resolved");
-            });
-        });
-      });
-    });
-  };
-  
-  const stage3Promise = () => {
-    return new Promise((resolve, reject) => {
-      step3Promise().then((message) => {
-        console.log(message);
-        logPromise("STAGE 3(2)- finished fetching the plant").then((message) => {
-          console.log(message);
-          step3Promise()
-            .then((message) => {
-              console.log(message);
-            })
-            .then(() => {
-              resolve("stage 3 is resolved");
-            });
-        });
-      });
-    });
-  };
-  
-  const stage4Promise = () => {
-    return new Promise((resolve, reject) => {
-      step3Promise().then((message) => {
-        console.log(message);
-        logPromise("STAGE 4(2)- Going to insert the plant").then((message) => {
-          console.log(message);
-          step3Promise()
-            .then((message) => {
-              console.log(message);
-            })
-            .then(() => {
-              resolve("stage 4 is resolved");
-            });
-        });
-      });
-    });
-  };
-  
-  const stage5Promise = () => {
-    return new Promise((resolve, reject) => {
-      step3Promise().then((message) => {
-        console.log(message);
-        logPromise("STAGE 5(2)- Inserting the plant").then((message) => {
-          console.log(message);
-          step3Promise()
-            .then((message) => {
-              console.log(message);
-            })
-            .then(() => {
-              resolve("stage 5 is resolved");
-            });
-        });
-      });
-    });
-  };
-  
-  const stage6Promise = () => {
-    return new Promise((resolve, reject) => {
-      step3Promise().then((message) => {
-        console.log(message);
-        logPromise("STAGE 6(2)- Finished inserting into mydb").then((message) => {
-          console.log(message);
-          step3Promise()
-            .then((message) => {
-              console.log(message);
-            })
-            .then(() => {
-              resolve("stage 6 is resolved");
-            });
-        });
-      });
-    });
-  };
-  
-  const stages = () => {
-    return new Promise((resolve, reject) => {
-      stage1Promise().then((message) => {
-        console.log(message);
-        stage2Promise().then((message) => {
-          console.log(message);
-          stage3Promise().then((message) => {
+  });
+};
+
+const stage2Promise = (t1) => {
+  return new Promise((resolve, reject) => {
+    step3Promise(t1).then((message) => {
+      console.log(message);
+      fetchFromDev(295).then((rows) => {
+        if (rows.length === 0) {
+          logPromise("STAGE 2- no such plant id found").then((message) => {
             console.log(message);
-            stage4Promise().then((message) => {
+            const t2 = Date.now() % 2000;
+            step3Promise(t2).then((message) => {
               console.log(message);
-              stage5Promise().then((message) => {
-                console.log(message);
-                stage6Promise()
-                  .then((message) => {
-                    console.log(message);
-                  })
-                  .then(() => {
-                    resolve("All stages are resolved");
-                  })
-                  .catch((error) => {
-                    reject("error", error);
-                  });
-              });
+              reject({ message: "stage 2 is rejected" });
             });
+          });
+        } else {
+          const row = rows[0];
+          logPromise(`STAGE 2(2)- fetching the plant id ${row.id}`).then((message) => {
+            console.log(message);
+            const t2 = Date.now() % 2000;
+            step3Promise(t2).then((message) => {
+              console.log(message);
+              resolve({ message: "stage 2 is resolved", t2, row });
+            });
+          });
+        }
+      });
+    });
+  });
+};
+
+const stage3Promise = (t2) => {
+  return new Promise((resolve, reject) => {
+    step3Promise(t2).then((message) => {
+      console.log(message);
+      logPromise("STAGE 3(2)- finished fetching the plant").then((message) => {
+        console.log(message);
+        const t3 = Date.now() % 2000;
+        step3Promise(t3).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 3 is resolved", t3 });
+        });
+      });
+    });
+  });
+};
+
+const stage4Promise = (t3) => {
+  return new Promise((resolve, reject) => {
+    step3Promise(t3).then((message) => {
+      console.log(message);
+      logPromise("STAGE 4(2)- Going to insert the plant").then((message) => {
+        console.log(message);
+        const t4 = Date.now() % 2000;
+        step3Promise(t4).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 4 is resolved", t4 });
+        });
+      });
+    });
+  });
+};
+
+const stage5Promise = (t4, row) => {
+  return new Promise((resolve, reject) => {
+    step3Promise(t4).then((message) => {
+      console.log(message);
+      insertIntoMydb(row).then(() => {
+        logPromise(`STAGE 5(2)- Inserting the plant id ${row.id}`).then((message) => {
+          console.log(message);
+          const t5 = Date.now() % 2000;
+          step3Promise(t5).then((message) => {
+            console.log(message);
+            resolve({ message: "stage 5 is resolved", t5 });
           });
         });
       });
     });
-  };
-  
-  stages().then((message) => {
-    console.log(message);
   });
-  
+};
+
+const stage6Promise = (t5) => {
+  return new Promise((resolve, reject) => {
+    step3Promise(t5).then((message) => {
+      console.log(message);
+      logPromise("STAGE 6(2)- Finished inserting into mydb").then((message) => {
+        console.log(message);
+        const t6 = Date.now() % 2000;
+        step3Promise(t6).then((message) => {
+          console.log(message);
+          resolve({ message: "stage 6 is resolved" });
+        });
+      });
+    });
+  });
+};
+
+const stages = () => {
+  return new Promise((resolve, reject) => {
+    stage1Promise().then(({ message: message, t1 }) => {
+      console.log(message);
+      stage2Promise(t1)
+        .then(({ message: message, t2, row }) => {
+          console.log(message);
+          stage3Promise(t2).then(({ message: message, t3 }) => {
+            console.log(message);
+            stage4Promise(t3).then(({ message: message, t4 }) => {
+              console.log(message);
+              stage5Promise(t4, row).then(({ message: message, t5 }) => {
+                console.log(message);
+                stage6Promise(t5, row).then(({ message: message }) => {
+                  console.log(message);
+                  resolve("All stages are resolved");
+                });
+              });
+            });
+          });
+        })
+        .catch((error) => {
+          console.log("stage 2 error", error);
+          reject(error);
+        });
+    });
+  });
+};
+
+stages()
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
