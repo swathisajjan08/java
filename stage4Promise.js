b029c9f65adcc5ebc4965d9cd6f7536948670911
swathi sajjan <swathi.novaglobal.in>
2025-06-02 09:34:52 +0530

    stagePromise made changes

---
+++ stage4Promise.js
@@ -0,0 +1,44 @@
+const delayPromise = () => {
+  return new Promise((delayResolve, reject) => {
+    const t1 = Date.now() % 2000;
+    setTimeout(() => {
+      delayResolve(`STAGE 4(1) - waiting for ${t1}ms before logging`);
+    }, t1);
+  });
+};
+
+const logPromise = (message) => {
+  return new Promise((logResolve, reject) => {
+    logResolve(message);
+  });
+};
+
+const thirdPromise = () => {
+  return new Promise((thirdResolve, reject) => {
+    const t2 = Date.now() % 2000;
+    setTimeout(() => {
+      thirdResolve(`STAGE 4(3) - waiting for time ${t2}ms after logging `);
+    }, t2);
+  });
+};
+
+const stage4Promise = () => {
+  return new Promise((resolve, reject) => {
+    delayPromise().then((message) => {
+      console.log(message);
+      logPromise("STAGE 4(2)- Going to insert the plant").then((message) => {
+        console.log(message);
+        thirdPromise()
+          .then((message) => {
+            console.log(message);
+          })
+          .then(() => {
+            resolve("stage 4 is resolved");
+          });
+      });
+    });
+  });
+};
+stage4Promise().then((message) => {
+  console.log(message);
+});
