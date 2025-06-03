4205aef62146be38945080888d1dd6d1e6eb168b
swathi sajjan <swathi.novaglobal.in>
2025-06-03 09:43:06 +0530

    plantNested made changes

--- plantNested.js
+++ plantNested.js
@@ -94,44 +94,44 @@ const step3Promise = (delay) => {
   });
 };
 
-const stage1Promise = () => {
+const stage1Promise = (id) => {
   return new Promise((resolve, reject) => {
     delayPromise(500).then((message) => {
       console.log(message);
-      logPromise("STAGE1(2) - Going to fetch from dev db").then((message) => {
+      logPromise(`${id}- STAGE1(2) - Going to fetch plant id ${id}`).then((message) => {
         console.log(message);
         const t1 = Date.now() % 2000;
         step3Promise(t1).then((message) => {
           console.log(message);
-          resolve({ message: "stage 1 is resolved", t1 });
+          resolve({ message: `stage 1 is resolved for plant id ${id}`, t1 });
         });
       });
     });
   });
 };
 
-const stage2Promise = (t1) => {
+const stage2Promise = (t1, id) => {
   return new Promise((resolve, reject) => {
     step3Promise(t1).then((message) => {
       console.log(message);
-      fetchFromDev(295).then((rows) => {
+      fetchFromDev(id).then((rows) => {
         if (rows.length === 0) {
-          logPromise("STAGE 2- no such plant id found").then((message) => {
+          logPromise(`${id}-STAGE 2- no such plant id ${id} found`).then((message) => {
             console.log(message);
             const t2 = Date.now() % 2000;
             step3Promise(t2).then((message) => {
               console.log(message);
-              reject({ message: "stage 2 is rejected" });
+              reject({ message: `stage 2 is rejected for plant id ${id}` });
             });
           });
         } else {
           const row = rows[0];
-          logPromise(`STAGE 2(2)- fetching the plant id ${row.id}`).then((message) => {
+          logPromise(`${id}-STAGE 2(2)- fetching the plant id ${row.id}`).then((message) => {
             console.log(message);
             const t2 = Date.now() % 2000;
             step3Promise(t2).then((message) => {
               console.log(message);
-              resolve({ message: "stage 2 is resolved", t2, row });
+              resolve({ message: `stage 2 is resolved for plant id ${id}`, t2, row });
             });
           });
         }
@@ -140,49 +140,49 @@ const stage2Promise = (t1) => {
   });
 };
 
-const stage3Promise = (t2) => {
+const stage3Promise = (t2, id) => {
   return new Promise((resolve, reject) => {
     step3Promise(t2).then((message) => {
       console.log(message);
-      logPromise("STAGE 3(2)- finished fetching the plant").then((message) => {
+      logPromise(`${id}-STAGE 3(2)- finished fetching the plant id ${id}`).then((message) => {
         console.log(message);
         const t3 = Date.now() % 2000;
         step3Promise(t3).then((message) => {
           console.log(message);
-          resolve({ message: "stage 3 is resolved", t3 });
+          resolve({ message: `stage 3 is resolved for plant id ${id}`, t3 });
         });
       });
     });
   });
 };
 
-const stage4Promise = (t3) => {
+const stage4Promise = (t3, id) => {
   return new Promise((resolve, reject) => {
     step3Promise(t3).then((message) => {
       console.log(message);
-      logPromise("STAGE 4(2)- Going to insert the plant").then((message) => {
+      logPromise(`${id}-STAGE 4(2)- Going to insert the plant id ${id}`).then((message) => {
         console.log(message);
         const t4 = Date.now() % 2000;
         step3Promise(t4).then((message) => {
           console.log(message);
-          resolve({ message: "stage 4 is resolved", t4 });
+          resolve({ message: `stage 4 is resolved for plant id ${id}`, t4 });
         });
       });
     });
   });
 };
 
-const stage5Promise = (t4, row) => {
+const stage5Promise = (t4, row, id) => {
   return new Promise((resolve, reject) => {
     step3Promise(t4).then((message) => {
       console.log(message);
       insertIntoMydb(row).then(() => {
-        logPromise(`STAGE 5(2)- Inserting the plant id ${row.id}`).then((message) => {
+        logPromise(`${id}-STAGE 5(2)- Inserting the plant id ${row.id}`).then((message) => {
           console.log(message);
           const t5 = Date.now() % 2000;
           step3Promise(t5).then((message) => {
             console.log(message);
-            resolve({ message: "stage 5 is resolved", t5 });
+            resolve({ message: `stage 5 is resolved for plant id ${id}`, t5 });
           });
         });
       });
@@ -190,55 +190,88 @@ const stage5Promise = (t4, row) => {
   });
 };
 
-const stage6Promise = (t5) => {
+const stage6Promise = (t5, id) => {
   return new Promise((resolve, reject) => {
     step3Promise(t5).then((message) => {
       console.log(message);
-      logPromise("STAGE 6(2)- Finished inserting into mydb").then((message) => {
+      logPromise(`${id}-STAGE 6(2)- Finished inserting into mydb for plant id ${id}`).then((message) => {
         console.log(message);
         const t6 = Date.now() % 2000;
         step3Promise(t6).then((message) => {
           console.log(message);
-          resolve({ message: "stage 6 is resolved" });
+          resolve({ message: `stage 6 is resolved for plant id ${id}` });
         });
       });
     });
   });
 };
-
-const stages = () => {
-  return new Promise((resolve, reject) => {
-    stage1Promise().then(({ message: message, t1 }) => {
-      console.log(message);
-      stage2Promise(t1)
-        .then(({ message: message, t2, row }) => {
+const main = (id) => {
+  const stages = (id) => {
+    return new Promise((resolve, reject) => {
+      stage1Promise(id)
+        .then(({ message: message, t1 }) => {
           console.log(message);
-          stage3Promise(t2).then(({ message: message, t3 }) => {
-            console.log(message);
-            stage4Promise(t3).then(({ message: message, t4 }) => {
+          stage2Promise(t1, id)
+            .then(({ message: message, t2, row }) => {
               console.log(message);
-              stage5Promise(t4, row).then(({ message: message, t5 }) => {
-                console.log(message);
-                stage6Promise(t5, row).then(({ message: message }) => {
+              stage3Promise(t2, id)
+                .then(({ message: message, t3 }) => {
                   console.log(message);
-                  resolve("All stages are resolved");
+                  stage4Promise(t3, id)
+                    .then(({ message: message, t4 }) => {
+                      console.log(message);
+                      stage5Promise(t4, row, id)
+                        .then(({ message: message, t5 }) => {
+                          console.log(message);
+                          stage6Promise(t5, id)
+                            .then(({ message: message }) => {
+                              console.log(message);
+                              resolve("All stages are resolved");
+                            })
+                            .catch((error) => {
+                              console.log(error);
+                              reject(error);
+                            });
+                        })
+                        .catch((error) => {
+                          console.log(error);
+                          reject(error);
+                        });
+                    })
+                    .catch((error) => {
+                      console.log(error);
+                      reject(error);
+                    });
+                })
+                .catch((error) => {
+                  console.log(error);
+                  reject(error);
                 });
-              });
+            })
+            .catch((error) => {
+              console.log("stage 2 error", error);
+              reject(error);
             });
-          });
         })
         .catch((error) => {
-          console.log("stage 2 error", error);
+          console.log(error);
           reject(error);
         });
     });
-  });
-};
+  };
 
-stages()
-  .then((message) => {
-    console.log(message);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  stages(id)
+    .then((message) => {
+      console.log(message);
+      if (id < 295) {
+        main(id + 1);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      if (id < 295) {
+        main(id + 1);
+      }
+    });
+};
+main(293);
