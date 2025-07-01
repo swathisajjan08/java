1257ac9f6ded5025010adcfb0ebb05a66c8ee172
swathisajjan08 <swathi@novaglobal.in>
2025-07-01 14:58:23 +0530

    Update App.js

--- react/loginform/src/App.js
+++ react/loginform/src/App.js
@@ -12,7 +12,7 @@ function App() {
     event.preventDefault();
 
     try {
-      const response = await fetch("https://loginbackend-2uec.onrender.com", {
+      const response = await fetch("https://loginbackend-2uec.onrender.com/loginpage", {
         method: "POST",
         headers: { 
           "Content-Type": "application/json",
